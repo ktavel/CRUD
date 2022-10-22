f58370e28484ebf8632231ee18b02c96069d31d0
Kassidy Tavel <tavelkassidy@gmail.com>
2022-10-22 18:16:02 -0500

    new html

--- CRUD Project/script.js
+++ CRUD Project/script.js
@@ -3,11 +3,12 @@ let selectedRow = null
 function onFormSubmit(){
     if(validate()) {
     let formData = readFormData();
-    if (let selectedRow == null);
+    if (selectedRow == null);
     insertNewRecord(formData);
-    else 
-    updateRecord(formData);
+    else {
+    if(updateRecord(formData));
     resetForm();
+        }
     }
 }
 
@@ -15,54 +16,42 @@ function readFormData() {
     let formData= {};
     formData['bikeName'] = document.getElementById('bikeName').value;
     formData['attribute'] = document.getElementById('attribute').value;
-    formData['feature'] = document.getElementById('feature').value;
-    formData['quality'] = document.getElementById('quality').value;
-    formData['trait'] = document.getElementById('trait').value;
+    formData['age'] = document.getElementById('age').value;
     return formData;
 }
 
 function insertNewRecord(data) {
-    let table = document.getElementById('bike list').getElementsByTagName('tbody')[0];
+    let table = document.getElementById('bikeList').getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.bikeName;
     cell2 = newRow.insertCell(1);
     cell2.innerHTML = data.attribute;
-    cell3 =newRow.insertCell(2);
-    cell3.innerHTML = data.feature;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.quality;
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = data.trait;
-    cell6 = newRow.insertCell(5);
-    cell6.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.age;
+    cell3 = newRow.insertCell(5);
+    cell3.innerHTML = `<a onClick="onEdit(this)">Edit</a>
     |   |   |   |   | <a onClick="onDelete(this)">Delete</a>`;
 }
 
 function resetForm() {
     document.getElementById('bikeName').value = '';
     document.getElementById('attribute').value = '';
-    document.getElementById('feature').value = '';
-    document.getElementById('quality').value = '';
-    document.getElementById('trait ').value = '';
+    document.getElementById('age').value = '';
     let selectedRow = null
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
     document.getElementById('bikeName').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('attribute').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('feature').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('quality').value = selectedRow.cells[0].innerHTML;
-    document.getElementById('trait').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('attribute').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('age').value = selectedRow.cells[2].innerHTML;
 }
 
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.bikeName;
-    selectedRow.cells[0].innerHTML = formData.attribute;
-    selectedRow.cells[0].innerHTML = formData.feature;
-    selectedRow.cells[0].innerHTML = formData.quality;
-    selectedRow.cells[0].innerHTML = formData.trait;
+    selectedRow.cells[1].innerHTML = formData.attribute;
+    selectedRow.cells[2].innerHTML = formData.age;
 }
 
 function onDelete(td) {
@@ -82,7 +71,7 @@ function validate() {
     else {
         isValid = true;
         if (!document.getElementById('bikeNameValidationError').classList.contains('hide'))
-        document.getElementById('bikeNameValidationError').classList.add('hide');
+             document.getElementById('bikeNameValidationError').classList.add('hide');
     }
     return isValid;
 }
