748fb3e6bb56e76d7fc1d437444097fa1f835568
Kassidy Tavel <tavelkassidy@gmail.com>
2022-10-22 16:26:43 -0500

    updated js

--- CRUD Project/script.js
+++ CRUD Project/script.js
@@ -1,6 +1,14 @@
+let selectedRow = null
+
 function onFormSubmit(){
+    if(validate()) {
     let formData = readFormData();
+    if (let selectedRow == null);
     insertNewRecord(formData);
+    else 
+    updateRecord(formData);
+    resetForm();
+    }
 }
 
 function readFormData() {
@@ -27,6 +35,54 @@ function insertNewRecord(data) {
     cell5 = newRow.insertCell(4);
     cell5.innerHTML = data.trait;
     cell6 = newRow.insertCell(5);
-    cell6.innerHTML = `<a>Edit</a>
-    |   |   |   |   | <a>Delete</a>`;
+    cell6.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+    |   |   |   |   | <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById('bikeName').value = '';
+    document.getElementById('attribute').value = '';
+    document.getElementById('feature').value = '';
+    document.getElementById('quality').value = '';
+    document.getElementById('trait ').value = '';
+    let selectedRow = null
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('bikeName').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('attribute').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('feature').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('quality').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('trait').value = selectedRow.cells[0].innerHTML;
+}
+
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.bikeName;
+    selectedRow.cells[0].innerHTML = formData.attribute;
+    selectedRow.cells[0].innerHTML = formData.feature;
+    selectedRow.cells[0].innerHTML = formData.quality;
+    selectedRow.cells[0].innerHTML = formData.trait;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure you want to delete this record?')){
+row = td.parentElement.parentElement;
+document.getElementById('bikeList').deleteRow(row.rowIndex);
+resetForm();
+    }
+}
+
+function validate() {
+    isValid = true;
+    if (document.getElementById('bikeName').value == '') {
+        isValid = false;
+        document.getElementById('bikeNameValidationError').classList.remove('hide');
+    }
+    else {
+        isValid = true;
+        if (!document.getElementById('bikeNameValidationError').classList.contains('hide'))
+        document.getElementById('bikeNameValidationError').classList.add('hide');
+    }
+    return isValid;
 }
